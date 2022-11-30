a69689bbffd2486e617cf744e7841e11841b5d72
Oscar Fernandez <oscarferna08@gmail.com>
2022-11-30 19:46:33 +0100

    Update PasswordPolicy.ps1

--- UT3/PasswordPolicy.ps1
+++ UT3/PasswordPolicy.ps1
@@ -1,11 +1,9 @@
-#Establecer política de contraseñas con comandos NET
-#Eemplo: Longitud mínima 8, tiempo de cambio de contraseña entre 2 y 60 días, bloqueo de cuenta con 3 intentos erróneos.
 
 #Bloqueo de cuentas 
 #Se configura para que se bloquee si se falla al intento 5.
 net accounts /lockoutthreshold:5
 
-#Duración del bloqueo 24 horas
+#Duración del bloqueo 2 horas
 net accounts /lockoutduration:120
 
 #Ventana de bloqueo (tiempo en minutos antes de que se restablezca el contador de intentos fallidos de contraseña )
@@ -20,8 +18,16 @@ net accounts /minpwage:2
 #El numero de contraseñas recordadas
 net accounts /UNIQUEPW:5
 
+
+
 #Establecer la longitud mínima del password, ejemplo: 8
 net accounts /minpwlen:8
 
 #Para consultar la configuración de los parámetros establecidos
-net accounts 
+net accounts
+
+#Habilitamos que la contraseña tiene que tener una complejidad alta
+secedit /export /cfg c:\secpol.cfg
+(gc C:\secpol.cfg).replace("PasswordComplexity = 0", "PasswordComplexity = 1") | Out-File C:\secpol.cfg
+secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
+rm -force c:\secpol.cfg -confirm:$false

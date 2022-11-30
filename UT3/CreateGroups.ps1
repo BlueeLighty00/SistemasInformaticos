dcef05e565c1d6d3ae37d97d6d26932f49067362
Oscar Fernandez <oscarferna08@gmail.com>
2022-11-30 19:46:58 +0100

    Update CreateGroups.ps1

--- UT3/CreateGroups.ps1
+++ UT3/CreateGroups.ps1
@@ -1,6 +1,7 @@
 # Creación de grupos de forma masiva.
-# Ayuda: https://learn.microsoft.com/es-es/powershell/module/microsoft.powershell.localaccounts/new-localgroup?view=powershell-5.1
+
 $file_groups=Import-Csv -Path Grupos.csv 
 foreach ($group in $file_groups) { 
   New-LocalGroup -Name $group.nombre -Description $group.Descripcion
 }
+
